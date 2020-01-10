287fc083c0aaf23ad7afc5ae83832113cc1bf46c
Jared <jaredclark11@gmail.com>
2020-01-10 14:15:22 -0800

    Tank, Rotation, movement

---
+++ Target Practice/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookOnObject= playerObject.position - transform.position;
+        transform.forward = lookOnObject.normalized;
+    }
+}
--- Target Practice/Assets/Scripts/Movement.cs
+++ Target Practice/Assets/Scripts/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private float speed = 40f;
+    public int speed=10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +15,30 @@ public class Movement : MonoBehaviour
     void Update()
     {
         Rigidbody body= GetComponent<Rigidbody>();
-        if (Input.GetKey(KeyCode.A))   
-            //body.AddForce(Vector3.left);
-            body.velocity=new Vector3(-1, 0, 0) * speed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.W))   
-            //body.AddForce(Vector3.forward);
-            body.velocity=new Vector3(0, 0, 1) * speed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.S))   
-            //body.AddForce(Vector3.back);
-            body.velocity=new Vector3(0, 0, -1) * speed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.D))   
-            //body.AddForce(Vector3.right);
-            body.velocity=new Vector3(1, 0, 0) * speed * Time.deltaTime;
         
+        if (Input.GetKey(KeyCode.A))
+        {   
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(Vector3.left);
+        }
+
+        else if (Input.GetKey(KeyCode.W))  
+        { 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.S)) 
+        {  
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(Vector3.back);
+        }
+
+        else if (Input.GetKey(KeyCode.D)) 
+        {  
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(Vector3.right);
+        }
+
     }
 }
