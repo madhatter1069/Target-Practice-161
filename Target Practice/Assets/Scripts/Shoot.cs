4dc87a16f9bf83c731017f00fcf005fc17cfe5c3
Jared <jaredclark11@gmail.com>
2020-01-11 23:48:33 -0800

    letters arena shooting
    shoot every 2 seconds
    walls are now clear 
    letters to see the directions

--- Target Practice/Assets/Scripts/Shoot.cs
+++ Target Practice/Assets/Scripts/Shoot.cs
@@ -6,23 +6,31 @@ public class Shoot : MonoBehaviour
 {
     //public GameObject projectile;
     public Rigidbody projectile;
-    public int bulletSpeed= 10;
+    public int bulletSpeed = 500;
+    private float timer = 0f;
+    public int shootTime = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer=shootTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer+=Time.deltaTime;
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            Rigidbody clone;
+            if (timer>shootTime){
+                Rigidbody clone;
             
-            clone = Instantiate(projectile, transform.position+ Vector3.up, transform.rotation);
-            clone.transform.parent= gameObject.transform;
-            clone.AddForce(transform.forward * bulletSpeed);
+                clone = Instantiate(projectile, transform.position+ Vector3.up, transform.rotation);
+                timer= timer-shootTime;
+                clone.transform.parent= gameObject.transform;
+                clone.AddForce(transform.forward * bulletSpeed);
+            }
+
+            
 
             //clone.transform.Translate(Vector3.forward * bulletSpeed);
             //clone.velocity = transform.TransformDirection(Vector3.forward * bulletSpeed);
