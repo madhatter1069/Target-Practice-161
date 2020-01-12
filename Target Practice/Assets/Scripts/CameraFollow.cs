17e429054e56644bdd2fb674ded820617d33ea91
Jared <jaredclark11@gmail.com>
2020-01-11 22:21:09 -0800

    Shoot
    Shoot the bullet and it dies when it hits a wall

--- Target Practice/Assets/Scripts/CameraFollow.cs
+++ Target Practice/Assets/Scripts/CameraFollow.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform playerObject;
+    public float distfromObj= 10f;
+    public int camHeight = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,14 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookOnObject= playerObject.position - transform.position;
-        transform.forward = lookOnObject.normalized;
+        //Vector3 lookOnObject= playerObject.position - transform.position;
+        //transform.forward = lookOnObject.normalized;
+        transform.position=playerObject.position-playerObject.forward * distfromObj;
+        transform.LookAt(playerObject.position);
+        transform.position= new Vector3 (transform.position.x,
+                                        transform.position.y+ camHeight,
+                                        transform.position.z);
+
+        
     }
 }
---
+++ Target Practice/Assets/Scripts/Shoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    //public GameObject projectile;
+    public Rigidbody projectile;
+    public int bulletSpeed= 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Rigidbody clone;
+            
+            clone = Instantiate(projectile, transform.position+ Vector3.up, transform.rotation);
+            clone.transform.parent= gameObject.transform;
+            clone.AddForce(transform.forward * bulletSpeed);
+
+            //clone.transform.Translate(Vector3.forward * bulletSpeed);
+            //clone.velocity = transform.TransformDirection(Vector3.forward * bulletSpeed);
+            /*
+            Rigidbody body= GetComponent<Rigidbody>();
+            GameObject bullet= Instantiate(projectile) as GameObject;
+            bullet.transform.position=transform.position+Vector3.forward;
+
+            
+            
+            /*GameObject bullet = Instantiate(projectile, 
+                                            new Vector3(body.position.x,
+                                            body.position.y+3,
+                                            body.position.z) , 
+                                            Quaternion.identity); 
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+            */
+        }
+        
+    }
+}
